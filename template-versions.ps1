5d348f207075a9d550f36b2abf494e42293e4079
Aza Asim <aza@shift.software>
2024-02-22 13:25:04 +0300

    Rename AzureFunctionsAspNetCoreAuthorization to AzureFunctionsAspNetCoreAuthorizationVersion

--- template-versions.ps1
+++ template-versions.ps1
@@ -2,7 +2,7 @@
 
 $ShiftFrameworkVersion = $ShiftFrameworkGlobalSettings.Project.PropertyGroup.ShiftFrameworkVersion
 
-$AzureFunctionsAspNetCoreAuthorization = $ShiftFrameworkGlobalSettings.Project.PropertyGroup.AzureFunctionsAspNetCoreAuthorization
+$AzureFunctionsAspNetCoreAuthorizationVersion = $ShiftFrameworkGlobalSettings.Project.PropertyGroup.AzureFunctionsAspNetCoreAuthorizationVersion
 
 $TemplateJsonPath = "ShiftTemplates\content\Framework Project\.template.config\template.json"
 
@@ -10,6 +10,6 @@ $TempalteJsonContent = Get-Content -Path $TemplateJsonPath | ConvertFrom-Json
 
 $TempalteJsonContent.symbols.frameworkVersion.parameters.value = $ShiftFrameworkVersion
 
-$TempalteJsonContent.symbols.azureFunctionsAspNetCoreAuthorizationVersion.parameters.value = $AzureFunctionsAspNetCoreAuthorization
+$TempalteJsonContent.symbols.azureFunctionsAspNetCoreAuthorizationVersion.parameters.value = $AzureFunctionsAspNetCoreAuthorizationVersion
 
 $TempalteJsonContent | ConvertTo-Json -Depth 100 | Set-Content -Path $TemplateJsonPath	
